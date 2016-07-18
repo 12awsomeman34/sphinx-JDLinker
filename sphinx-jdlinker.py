2dd66eca987b5033abd55071d43da018c7aad25b
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2016-07-18 09:19:28 -0400

    Add LICENSE.txt file at the root of the project.

--- sphinx-jdlinker.py
+++ sphinx-jdlinker.py
@@ -6,30 +6,6 @@ Found an issue with sphinx-JDLinker? Report it to the sphinx-JDLinker GitHub rep
 Make sure to include a description of what went wrong, including the javadoc role used (i.e. the
 :javadoc:`some.package.SomeClass` thing), the actual displayed text output, and the actual javadoc link output, if
 possible.
-
-sphinx-JDLinker is licensed under the MIT License (MIT). A copy of the license may be found below:
-
-Copyright (c) 12AwsomeMan34 (https://github.com/12AwsomeMan34)
-Copyright (c) contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
 """
 from docutils import nodes, utils
 
