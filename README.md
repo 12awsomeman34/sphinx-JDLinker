2e40f12faa74b007e84708e617842336f1dc61d4
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2016-07-19 23:53:59 -0400

    Setup files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# sphinx-JDLinker
+
+A custom Sphinx extension that allows you to link to a JavaDoc website through your documentation.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+long_description="""
+A custom sphinx extension that will allow you to link to the JavaDocs for your project from your documentation.
+
+Please see the `GitHub repository <https://github.com/12AwsomeMan34/sphinx-JDLinker>`__ for more information.
+"""
+
+setup(
+    name='sphinx-JDLinker',
+    version='0.1',
+    license='MIT',
+    description='A sphinx extension designed to allow you to create links to a JavaDoc website from your sphinx'
+                ' documentation.',
+    long_description=long_description,
+    url='https://github.com/12AwsomeMan34/sphinx-JDLinker',
+    author='12AwsomeMan34',
+    author_email='aaronlingerfelt@yahoo.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Java',
+        'Programming Language :: Python',
+        'Topic :: Documentation :: Sphinx'
+    ],
+    keywords='javadoc link sphinx documentation'
+)
