197aa3b5053b19e9d79af0d006401d933f846328
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2016-07-24 12:49:35 -0400

    Actually do a better implementation of this.

--- jdlinker/__init__.py
+++ jdlinker/__init__.py
@@ -1,7 +1,7 @@
 from .jdlinker import javadoc_role
-from os import remove
+from os import remove, path
 
-__version = '1.0.8'
+__version = '1.0.9'
 
 
 def setup(app):
@@ -9,15 +9,10 @@ def setup(app):
     app.add_role('javadoc', javadoc_role)
     app.add_config_value('javadoc_links', [], 'env')
     app.add_config_value('javadoc_dump', False, 'env')
+
     # Unfortunately there isn't a reliable way to fetch the javadoc_dump config option from here, so we have to remove
     # the javadoc_dump.txt file and allow the javadoc role to re-generate it if the config option is set from there.
-    # We also stumble across the problem of if the file isn't there and we try to remove it. We have to swallow the
-    # error and move on from there.
-    try:
-        # Try to remove the javadoc_dump.txt file if it exists.
+    if path.isfile('javadoc_dump.txt'):
         remove('javadoc_dump.txt')
-    except Exception:
-        # If there is an error, ignore it.
-        pass
 
     return {'version': __version}
